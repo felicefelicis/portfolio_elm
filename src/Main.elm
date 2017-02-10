017c09677fce67391efed1c36b2359c917c5c1d6
Kathleen Blackburn <inwie.l@gmail.com>
2017-02-09 19:35:21 -0500

    ajoute du fichier html de base

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing ( .. )
 
 import Html exposing ( .. )
 
+import Html.Attributes exposing ( .. )
+
 
 
 -- MODEL
@@ -31,7 +33,42 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "WASSUP BITCHES" ] 
+    body []
+    [ nav [ id "navigationPrincipale" ]
+        [ ul []
+            [ li []
+                [ a [ href "#" ]
+                    [ text "PORTFOLIO" ]
+                ]
+            , li []
+                [ a [ href "#" ]
+                    [ text "COMPÉTENCES" ]
+                ]
+            , li []
+                [ a [ href "#" ]
+                    [ text "À PROPOS" ]
+                ]
+            ]
+        ]
+    , div [ id "entete" ]
+        []
+    , div [ class "nom" ]
+        [ h1 []
+            [ text "KATHLEEN" ]
+        , h1 []
+            [ text "BLACKBURN" ]
+        ]
+    , div [ id "portfolio" ]
+        [ h3 [ class "quote" ]
+            [ text "Parce qu'il faut parfois savoir être showoff" ]
+        , h2 [ class "sectionTitre" ]
+            [ text "Portfolio" ]
+        ]
+    , div [ id "competence" ]
+        []
+    , div [ id "joindre" ]
+        []
+    ] 
 
 
 
