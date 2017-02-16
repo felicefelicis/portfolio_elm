a5255924150d0fa022182b4b02bbad10b6c14e59
Kathleen Blackburn <inwie.l@gmail.com>
2017-02-15 21:17:07 -0500

    Mise en oeuvre de la selection aleatoire de la baniere et ajout de commentaires. Configuration du projet.

--- src/Main.elm
+++ src/Main.elm
@@ -1,38 +1,93 @@
-module Main exposing ( .. )
+module Main exposing ( Model, model, init, Msg, update, view, main )
+
+{-| Mon Portfolio.
+
+Suit le modèle établit par 
+  [`The elm architecture`](https://evancz.gitbooks.io/an-introduction-to-elm/content/architecture/)
+
+# App model
+@docs Model, model, init
+
+# App message
+@docs Msg
+
+# App update
+@docs update
+
+# App view
+@docs view
+
+# Lancement de l'application
+@docs main
+
+-}
+
+
 
 import Html exposing ( .. )
 
 import Html.Attributes exposing ( .. )
 
+import Random.Pcg as Random
+
 
 
 -- MODEL
+{-| Le modèle de notre application.
+Contient tout les états modifiables de notre programme.
+-}
+type alias Model =
+    { backgroundImage : Int
+    }
 
-type alias Model = List Int
 
+{-| Valeur initiale du modèle.
+On initialise `backgroundImage` à 1 uniquement afin de satisfaire le type `Model`
+puisque sa valeur sera automatiquement remplacée par une valeur aléatoire.
+-}
 model : Model
-model = []
+model =
+    { backgroundImage = 1
+    }
 
+
+backgroundGenerator : Random.Generator Int
+backgroundGenerator = Random.int 1 3
+
+
+{-| Initialise la programme avec le modèle initial et lance le message SetBgImage
+avec une valeur aléatoire.
+-}
 init : (Model, Cmd Msg)
-init = (model, Cmd.none)
+init = (model, Random.generate SetBgImage backgroundGenerator)
+
 
 
 -- MESSAGES
 
-type alias Msg = Never
+{-| Les messages de gérés par notre application.
+
+      * `SetBgImage n`: Change la valeur de `backgroundImage`
+-}
+type Msg = SetBgImage Int
 
 
 
 -- UPDATE
 
+{-| Transforme le modèle en fonction du message reçu. 
+-}
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    (model, Cmd.none)
+    case msg of
+        SetBgImage n -> ({ backgroundImage = n }, Cmd.none)
 
 
 
 --VIEW
 
+{-| Affichage du modèle
+-}
 view : Model -> Html Msg
 view model =
     body []
@@ -52,7 +107,7 @@ view model =
                 ]
             ]
         ]
-    , div [ id "entete" ]
+    , div [ id "entete", style [ ("backgroundImage" , "url(images/bg" ++ (toString model.backgroundImage) ++ ".jpeg)") ] ]
         []
     , div [ class "nom" ]
         [ h1 []
@@ -76,6 +131,8 @@ view model =
 
 -- MAIN
 
+{-| Lance l'application -}
+main : Program Never Model Msg
 main =
     program
         { init = init
